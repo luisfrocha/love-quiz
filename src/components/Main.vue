55d1f852ff910a99f4d74357d3800947ce3cc54a
Luis Fernando Rocha <luisfrocha@icloud.com>
2019-02-11 19:55:18 -0600

    Added Vuetify

---
+++ src/components/Main.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    Main
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  data() {
+    return {
+      questions: {
+        en: [
+          {
+            a: "I like to receive notes of affirmation from you",
+            e: "I like it when you hug me"
+          },
+          {
+            b: "I like to spend one-on-one time with you",
+            d: "I feel loved when you give me practical help"
+          },
+          {
+            c: "I like it when you give me gifst",
+            b: "I like taking long walks with you"
+          },
+          {
+            d: "I feel loved when you do things to help me",
+            e: "I feel loved when you hug or touch me"
+          },
+          {
+            e: "I feel loved when you hold me in your arms",
+            c: "I feel loved when I receive a gift from you"
+          }
+        ]
+      }
+    };
+  }
+};
+</script>
+
+<style scoped></style>
